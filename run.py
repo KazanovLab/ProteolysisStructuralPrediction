e9dba88c441c5f5ad3889d487286f689a41539b8
EugeneVlg02 <56604602+EugeneVlg02@users.noreply.github.com>
2024-10-30 13:57:57 +0300

    add updated "README.md" and "run.py" files

--- run.py
+++ run.py
@@ -13,37 +13,46 @@ log_file = os.path.join(result_path, "report.log")
 def main():
     print("######## START ########\n")
 
-    parser = argparse.ArgumentParser(description='### CutProba - the tool for predicting probabilities of proteolytic events in proteins (Structural model part) ###')
-    parser.add_argument("-i", "--input", help="PDB ID, or a full path to the file with a table where the first column is the PDB ID, and the second is the name of the chain. The value separator should be a comma. For example, '4GAW' or '/dir1/dir2/structures.txt'.", type=str)
-    parser.add_argument("-c", "--chain", help="A name of the chain, if you specify only one PDB ID or one PDB file. For example, 'A'.", type=str)
-    parser.add_argument("-f", "--file", help="A full path to the PDB file or a full path to the file with a table where the first column is a full path to the PDB file, and the second is the name of the chain. The value separator should be a comma. For example, '/dir1/dir2/4GAW.pdb' or '/dir1/dir2/structures.txt'.", type=str)
-    parser.add_argument("-l", "--locally", help="Run DSSP locally (specify '-l') or via server", action="store_true")
-    parser.add_argument("-v", "--visualise", help="Visualise total proteolytic score (specify '-v') or not", action="store_true")
+    parser = argparse.ArgumentParser(description='### The tool for predicting probabilities of proteolytic events in proteins by structure information (structures from PDB or AlphaFoldDB databases) ###')
+    parser.add_argument("-i", "--input", help="The structure ID from PDB or AlphaFoldDB databases, or a full path to the PDB file from PDB or AlphaFoldDB databases, or a full path to the file with a table where the first column is the structure ID, and the second is the name of the chain (You need specify '-' for AlphaFold structure). The value separator should be a comma. For example, 1) -i '4GAW' -c 'A' 2) -i 'AF-P00257-F1' 3) -i '/dir1/dir2/4GAW.pdb' -c 'A' 4) -i '/dir1/dir2/AF-P00257-F1.pdb' 5) -i '/dir1/dir2/structures.txt'. Also see directory 'input_examples' for a better understanding.", type=str)
+    parser.add_argument("-c", "--chain", help="A name of the chain, only for PDB ID or PDB file. For example, 'A'.", type=str)
+    parser.add_argument("-l", "--locally", help="Run DSSP program locally (specify '-l') or via server.", action="store_true")
+    parser.add_argument("-v", "--visualise", help="Visualise the total proteolytic score (specify '-v') or not.", action="store_true")
     args = parser.parse_args()
    
     print("######## Step 1 -- Generating of PDB file with separate chain ########")
     try:
-        if ((not (args.input is None)) and (not os.path.isfile(args.input))) and (not (args.chain is None)) and (args.file is None):
-            process1 = subprocess.run(["python", f"{script_path}/1_download_structure_with_sep_chain.py", f"-i={args.input}", f"-c={args.chain}"], capture_output=True, text=True, check=True)
-        elif ((not (args.input is None)) and os.path.isfile(args.input)) and (args.chain is None) and (args.file is None):
-            process1 = subprocess.run(["python", f"{script_path}/1_download_structure_with_sep_chain.py", f"-i={args.input}"], capture_output=True, text=True, check=True)
-            if 'ERROR' in process1.stdout:
-                print(process1.stdout)
-                sys.exit()
-        elif (args.input is None) and (not (args.chain is None)) and ((not (args.file is None)) and ('.pdb' in args.file)):
-            process1 = subprocess.run(["python", f"{script_path}/1_download_structure_with_sep_chain.py", f"-f={args.file}", f"-c={args.chain}"], capture_output=True, text=True, check=True)
-        elif (args.input is None) and (args.chain is None) and ((not (args.file is None)) and ('.pdb' not in args.file) and os.path.isfile(args.file)):
-            process1 = subprocess.run(["python", f"{script_path}/1_download_structure_with_sep_chain.py", f"-f={args.file}"], capture_output=True, text=True, check=True)
-            if 'ERROR' in process1.stdout:
-                print(process1.stdout)
-                sys.exit()
+        # ID #
+        if (not args.input is None) and (not os.path.isfile(args.input)):
+            # AlphaFold structure #
+            if 'AF-' in args.input:
+                process1 = subprocess.run(["python", f"{script_path}/1.download_structure_with_sep_chain.py", f"-i={args.input}"], capture_output=True, text=True, check=True)
+            # PDB structure #
+            else:
+                process1 = subprocess.run(["python", f"{script_path}/1.download_structure_with_sep_chain.py", f"-i={args.input}", f"-c={args.chain}"], capture_output=True, text=True, check=True)
+        # PDB file #
+        elif (not args.input is None) and os.path.isfile(args.input) and args.input.endswith('.pdb'):
+            # AlphaFold structure #
+            if 'AF-' in args.input:
+                process1 = subprocess.run(["python", f"{script_path}/1.download_structure_with_sep_chain.py", f"-i={args.input}"], capture_output=True, text=True, check=True)
+            # PDB structure #
+            else:
+                process1 = subprocess.run(["python", f"{script_path}/1.download_structure_with_sep_chain.py", f"-i={args.input}", f"-c={args.chain}"], capture_output=True, text=True, check=True)
+        # TXT file #
+        elif (not args.input is None) and os.path.isfile(args.input) and args.input.endswith('.txt'):
+            process1 = subprocess.run(["python", f"{script_path}/1.download_structure_with_sep_chain.py", f"-i={args.input}"], capture_output=True, text=True, check=True)
         else:
-            print("ERROR: Possibly, your input of PDB ID (-i), chain (-c) or PDB file (-f) is uncorrected! Check with templates:\n1: -i 4GAW -c A\n2: -i /dir1/dir2/structures.txt\n3: -f /dir1/dir2/name.pdb -c A\n4: -f /dir1/dir2/pdb_structures.txt\n")
+            print("ERROR: Possibly, your input parameters (-i or -c) are uncorrected! Check with templates:\n1: -i 4GAW -c A\n2: -i AF-P00257-F1\n3: -i /dir1/dir2/4GAW.pdb -c A\n4: -i /dir1/dir2/AF-P00257-F1.pdb\n5: -i /dir1/dir2/structures.txt\n")
+            sys.exit()
+        
+        if 'ERROR' in process1.stdout:
+            print(process1.stdout)
             sys.exit()
     except subprocess.CalledProcessError as e:
         print()
         print(e.stderr)
         sys.exit()
+    
     # Checkpoint 1 #
     if (len(os.listdir(result_path)) == 1) and (os.path.exists(log_file)):
         print("\n******** WARNING ********\nNo structures are downloaded!\n")
@@ -51,9 +60,9 @@ def main():
     print("######## Step 1 -- WELL DONE! ########\n")
 
     print("######## Step 2 -- Preprocessing  ########")
-    print("######## Step 2.1. -- Ligand removing ########")
+    print("######## Step 2.1. -- Ligands and solvent removing ########")
     try:
-        process2 = subprocess.run(["chimera", "--nogui", f"--script={script_path}/2_del_ligands_pyv2.py"], capture_output=True, text=True, check=True)
+        process2 = subprocess.run(["chimera", "--nogui", f"--script={script_path}/2.del_ligands_solvent_pyv2.py"], capture_output=True, text=True, check=True)
         if "Error while processing" in process2.stderr:
             error_index = process2.stderr.find("Error while processing")
             print(process2.stderr[error_index:])
@@ -65,7 +74,7 @@ def main():
 
     print("######## Step 2.2. -- PDB-file parsing ########")
     try:
-        process3 = subprocess.run(["python", f"{script_path}/3_parse_pdb.py"], capture_output=True, text=True, check=True)
+        process3 = subprocess.run(["python", f"{script_path}/3.parse_pdb.py"], capture_output=True, text=True, check=True)
         if 'ERROR' in process3.stdout:
             print(process3.stdout)
             sys.exit()
@@ -78,12 +87,12 @@ def main():
     print("######## Step 3 -- Generating the DSSP file ########")
     try:
         if args.locally:
-            process4 = subprocess.run(["python", f"{script_path}/4_create_dssp.py", "-l"], capture_output=True, text=True, check=True)
+            process4 = subprocess.run(["python", f"{script_path}/4.generate_dssp_file.py", "-l"], capture_output=True, text=True, check=True)
         else:
-            process4 = subprocess.run(["python", f"{script_path}/4_create_dssp.py"], capture_output=True, text=True, check=True)
-            if "ERROR" in process4.stdout:
-                print(process4.stdout)
-                sys.exit()
+            process4 = subprocess.run(["python", f"{script_path}/4.generate_dssp_file.py"], capture_output=True, text=True, check=True)
+        if "ERROR" in process4.stdout:
+            print(process4.stdout)
+            sys.exit()
     except subprocess.CalledProcessError as e:
         print()
         print(e.stderr)
@@ -92,7 +101,7 @@ def main():
     
     print("######## Step 4 -- Extracting features ########")
     try:
-        process5 = subprocess.run(["python", f"{script_path}/5_extract_features.py"], capture_output=True, text=True, check=True)
+        process5 = subprocess.run(["python", f"{script_path}/5.extract_features.py"], capture_output=True, text=True, check=True)
     except subprocess.CalledProcessError as e:
         print()
         print(e.stderr)
@@ -101,7 +110,7 @@ def main():
     
     print("######## Step 5 -- Normalisation of data ########")
     try:
-        process6 = subprocess.run(["python", f"{script_path}/6_norm_data.py"], capture_output=True, text=True, check=True)
+        process6 = subprocess.run(["python", f"{script_path}/6.norm_data.py"], capture_output=True, text=True, check=True)
     except subprocess.CalledProcessError as e:
         print()
         print(e.stderr)
@@ -110,7 +119,7 @@ def main():
     
     print("######## Step 6 -- Predicting structural scores ########")
     try:
-        process7 = subprocess.run(["python", f"{script_path}/7_get_structural_score.py"], capture_output=True, text=True, check=True)
+        process7 = subprocess.run(["python", f"{script_path}/7.generate_structural_score.py"], capture_output=True, text=True, check=True)
     except subprocess.CalledProcessError as e:
         print()
         print(e.stderr)
@@ -120,7 +129,7 @@ def main():
     if args.visualise:
         print("######## Step 7 -- Visualising scores ########")
         try:
-            process8 = subprocess.run(["chimera", "--nogui", f"--script={script_path}/map_scores_pyv2.py"], capture_output=True, text=True, check=True)
+            process8 = subprocess.run(["chimera", "--nogui", f"--script={script_path}/8.map_scores_pyv2.py"], capture_output=True, text=True, check=True)
             if "Error while processing" in process8.stderr:
                 error_index = process8.stderr.find("Error while processing")
                 print(process8.stderr[error_index:])
